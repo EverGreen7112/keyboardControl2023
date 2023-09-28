5d88af11e32500c719ed37660c4a90e4c1539f3e
senpairo <nadavh2611@gmail.com>
2023-09-28 12:06:05 +0300

    changed the change me's, added requirements

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import json
 PORT = 5801
 dict_to_send = {}
 class Action:
-    def __init__(self, button: str, name: str):
+    def __init__(self, name: str, button: str):
         self.button = button
         self.name = name
     
     def register_action(self):
         dict_to_send[self.name] = keyboard.is_pressed(self.button)
+        
 
 
 def send():
@@ -21,28 +22,30 @@ def send():
 
 # TODO: change all change me's
 def main():
-    all_actions = [ Action("ARM_TO_0", "change me"),
-                    Action("ARM_TO_CONE_3", "change me"),
-                    Action("ARM_TO_CONE_2", "change me"),
-                    Action("ARM_TO_CONE_1", "change me"),
+    all_actions = [ Action("ARM_TO_0", "backspace"),
+                    Action("ARM_TO_CONE_3", "q"),
+                    Action("ARM_TO_CONE_2", "a"),
+                    Action("ARM_TO_CONE_1", "z"),
 
-                    Action("ARM_TO_CUBE_3", "change me"),
-                    Action("ARM_TO_CUBE_2", "change me"),
-                    Action("ARM_TO_CUBE_1", "change me"),
+                    Action("ARM_TO_CUBE_3", "w"),
+                    Action("ARM_TO_CUBE_2", "s"),
+                    Action("ARM_TO_CUBE_1", "x"),
 
-                    Action("ARM_COLLECT_CUBE_SHELF", "change me"),
-                    Action("ARM_COLLECT_CONE_SHELF", "change me"),
+                    Action("ARM_COLLECT_CUBE_SHELF", "o"),
+                    Action("ARM_COLLECT_CONE_SHELF", "p"),
+                    
+                    Action("GRIPPER_TO_CUBE", "9"),
+                    Action("GRIPPER_TO_CONE", "0"),
+                    Action("GRIPPER_TO_OPEN", "8"),
 
-                    Action("GRIPPER_TO_CUBE", "change me"),
-                    Action("GRIPPER_TO_CONE", "change me"),
-                    Action("GRIPPER_TO_CLOSED", "change me"),
-
-                    Action("GRIPPER_IN", "change me"),
-                    Action("GRIPPER_OUT", "change me")]
+                    Action("GRIPPER_IN", "f"),
+                    Action("GRIPPER_OUT", "h"),
+                    Action("GRIPPER_STOP", "g")]
     while True:
         for action in all_actions:
             action.register_action()
         send()
+        # print(dict_to_send.values()) # for debugging 
         
 
 if __name__ == '__main__':
